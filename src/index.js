732bafa4c3c01b333e1e6d004d8f275257a113bc
andrefangeloni <andrefangeloni@gmail.com>
2020-02-03 22:22:48 -0300

    Typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const cors = require('./cors');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
